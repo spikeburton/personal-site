c3ec8be648227a10c57b5a0671f67fca5edab1e6
spikeburton <ralphburton3@gmail.com>
2019-07-29 11:18:09 -0400

    add background/theme depending on environment

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,13 @@
+const { NODE_ENV } = process.env
+
+const colorSelector = env => {
+  return env === "production"
+    ? "#663399" // Purple
+    : env === "staging"
+    ? "#405ac1" // Blue
+    : "#c13f5e" // Red
+}
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -21,8 +31,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: colorSelector(NODE_ENV),
+        theme_color: colorSelector(NODE_ENV),
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
