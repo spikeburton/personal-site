b06500bd04f1f7ebdc40f9bdecee6a7fac5e9a03
spikeburton <ralphburton3@gmail.com>
2019-07-29 17:47:53 -0400

    add remark transformer plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,6 +38,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-emotion`,
+    `gatsby-transformer-remark`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
