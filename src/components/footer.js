07d7a540856742c2a2f349db81e0a2129a37722e
spikeburton <ralphburton3@gmail.com>
2019-07-30 00:01:29 -0400

    super secret

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,19 @@ import Social from "./social"
 const Footer = () => (
   <footer>
     <Social />
-    <h6>© {new Date().getFullYear()}, Spike Burton</h6>
+    <h6>
+      © {new Date().getFullYear()}, Spike Burton{" "}
+      <a
+        href="https://www.facebook.com/spikeburton"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Whaaa?!"
+      >
+        <span role="img" aria-label="plug">
+          🔌
+        </span>
+      </a>
+    </h6>
   </footer>
 )
 
