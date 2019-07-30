69164eccb60c3954311fc2ca93d03faf47095462
spikeburton <ralphburton3@gmail.com>
2019-07-30 00:29:19 -0400

    use markdown for about me

---
+++ src/data/about.md
@@ -0,0 +1,10 @@
+# About Me
+
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo,
+magnam nobis fuga quos sunt libero nostrum laboriosam numquam commodi
+dicta voluptate cupiditate placeat repellendus vero quidem odio possimus
+quisquam eveniet.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea incidunt ad
+fugit enim atque voluptatem nisi ex nesciunt, quis iste deserunt in
+facilis saepe labore quos ab aliquid repudiandae nam!
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,45 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 import { css } from "@emotion/core"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-export default () => (
-  <Layout>
-    <SEO title="Home" />
-    <div
-      css={css`
-        max-width: 200px;
-        margin: 0 auto 1.45rem;
-        @media (max-width: 768px) {
-          max-width: 150px;
-        }
-        @media (min-width: 1024px) {
-          max-width: 250px;
-        }
-        @media (min-width: 1440px) {
-          max-width: 300px;
-          margin: 0 0.95rem 3.85rem;
-        }
-      `}
-    >
-      <Image />
-    </div>
-    <h1>About Me</h1>
-    <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo,
-      magnam nobis fuga quos sunt libero nostrum laboriosam numquam commodi
-      dicta voluptate cupiditate placeat repellendus vero quidem odio possimus
-      quisquam eveniet.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea incidunt ad
-      fugit enim atque voluptatem nisi ex nesciunt, quis iste deserunt in
-      facilis saepe labore quos ab aliquid repudiandae nam!
-    </p>
-  </Layout>
-)
+export default () => {
+  const { about } = useStaticQuery(graphql`
+    query {
+      about: markdownRemark(fileAbsolutePath: { regex: "/data/about.md/" }) {
+        html
+      }
+    }
+  `)
+  console.log(about)
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      {/* Avatar */}
+      <div
+        css={css`
+          max-width: 200px;
+          margin: 0 auto 1.45rem;
+          @media (max-width: 768px) {
+            max-width: 150px;
+          }
+          @media (min-width: 1024px) {
+            max-width: 250px;
+          }
+          @media (min-width: 1440px) {
+            max-width: 300px;
+            margin: 0 0.95rem 3.85rem;
+          }
+        `}
+      >
+        <Image />
+      </div>
+      {/* About Me Section */}
+      <section dangerouslySetInnerHTML={{ __html: about.html }} />
+    </Layout>
+  )
+}
