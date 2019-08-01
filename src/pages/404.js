688b77fdcd5b04ec90197743f07954f5c9e201c1
spikeburton <ralphburton3@gmail.com>
2019-08-01 14:20:19 -0400

    add 404 text

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { css } from "@emotion/core"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -6,13 +7,32 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <h4>
-      <span role="img" aria-label="robot">
-        🤖
-      </span>{" "}
-      Beep, Boop. Whatcha doin over here?
-    </h4>
+    <section
+    // css={css`
+    //   text-align: center;
+    // `}
+    >
+      <h1
+        css={css`
+          color: hsla(180, 100%, 25%, 0.8);
+          display: inline-block;
+        `}
+      >
+        404
+      </h1>{" "}
+      <h6
+        css={css`
+          color: hsla(16, 100%, 47%, 1);
+          display: inline-block;
+        `}
+      >{`Not Found ¯\\_(ツ)_/¯`}</h6>
+      <h4>
+        {/* <span role="img" aria-label="robot">
+          🤖
+        </span>{" "} */}
+        Oops. Whatcha doin' over here?
+      </h4>
+    </section>
   </Layout>
 )
 
